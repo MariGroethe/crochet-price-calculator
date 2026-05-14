1cba9fb3dbbb014557344bb93cbb47beef9181a3
Mari Groethe <mari.groethe@gmail.com>
2026-05-14 13:04:53 -0600

    Initial commmit of crochet calculator

---
+++ android/app/src/main/kotlin/com/example/crochet_price_calc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.crochet_price_calc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CrochetApp());
+}
+
+class CrochetApp extends StatelessWidget {
+  const CrochetApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const CalculatorScreen());
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String hoursInput = '';
+  String materialsInput = '';
+
+  double totalPrice = 0.0;
+  double markup = 1.0;
+  double wage = 10.0;
+  List<double> wageOptions = [for (double i = 10.0; i <= 20.0; i += 5.0) i];
+
+  void _calculatePrice() {
+    double hours = double.tryParse(hoursInput) ?? 0.0;
+    double materials = double.tryParse(materialsInput) ?? 0.0;
+
+    setState(() {
+      totalPrice = (hours * wage) + (materials * markup);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Crochet Price Calculator')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            // hours input
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Hours Worked',
+                hintText: 'Enter total hours (e.g. 5)',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  hoursInput = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+
+            // materials input
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Material Costs',
+                hintText: 'Enter total material costs (e.g. 15)',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  materialsInput = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+
+            // markup Slider
+            Text('Markup: ${markup.toStringAsFixed(1)}x'),
+            Slider(
+              value: markup,
+              min: 1.0,
+              max: 5.0,
+              activeColor: const Color.fromARGB(255, 55, 93, 214),
+              inactiveColor: Colors.blueGrey,
+              thumbColor: const Color.fromARGB(255, 23, 60, 164),
+              divisions: 8,
+              label: markup.toStringAsFixed(1),
+              onChanged: (double newValue) {
+                setState(() {
+                  markup = newValue;
+                });
+              },
+            ),
+
+            // hourly wage dropdown
+            DropdownButton<double>(
+              value: wage,
+              focusColor: Colors.transparent,
+              items: wageOptions.map((double value) {
+                return DropdownMenuItem<double>(
+                  value: value,
+                  child: Text('\$$value per hour'),
+                );
+              }).toList(),
+              onChanged: (double? newValue) {
+                setState(() {
+                  wage = newValue!;
+                });
+              },
+            ),
+
+            // calculate button
+            ElevatedButton(
+              onPressed: _calculatePrice,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 15,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text('Calculate Price'),
+            ),
+            const SizedBox(height: 30),
+
+            // display result
+            Text(
+              'Total Price: \$${totalPrice.toStringAsFixed(2)}',
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
